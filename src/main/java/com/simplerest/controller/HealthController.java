9bc02c19926611e035ea1b9dbc623b9887bb0c6d
Gabrielluiz006 <gabrielcontato006@gmail.com>
2026-08-23 15:31:11 -0300

    ajuste no maven e controller

---
+++ src/main/java/com/simplerest/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.simplerest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    public ResponseEntity<Object> getHealth() {
+        return new ResponseEntity<>(
+                new HealthResponse("200", "Service is up and running"),
+                HttpStatus.OK
+        );
+    }
+
+    record HealthResponse(String httpStatus, String description) {
+
+    }
+}
